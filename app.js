d678821841ada2580e250dcc464e8e24559c162d
abbos97 <yourali97@gmail.com>
2023-01-11 19:43:35 +0900

    feat: mongoDB ulanish locic

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 
-console.log('Web server boshlandi');
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
-const http = require('http');
-const fs = require('fs');
+//const fs = require('fs');
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err)
-    } else {
-        user = JSON.parse(data)
-    }
-})
+
+
+
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//     if(err) {
+//         console.log("ERROR:", err)
+//     } else {
+//         user = JSON.parse(data)
+//     }
+// })
+
+
+const db = require("./server").db();
+
 
 //1. Kirish
 
@@ -42,10 +47,5 @@ app.get("/", function(req, res) {
     res.render("reja");
 })
 
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-})
-
+module.exports = app;
 
---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const mongodb = require("mongodb")
+
+let db;
+const connectionString = "mongodb+srv://abbos97DB:SknWiXvzIhPpIOVt@cluster0.joyqir0.mongodb.net/Reja?retryWrites=true&w=majority";
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true,}, (err, client) => {
+    if(err) console.log("ERROR on connection Mongodb");
+    else {
+        console.log("Mongodb connaction succed")
+        module.exports = client;
+        console.log(client)
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function() {
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+        })
+    }
+});
+
+
+
+
